07aea9937f7caf61ba038f52779610c6b4aabcbe
Russell Sabia <rjsabia@gmail.com>
2017-09-28 00:12:26 -0500

    intial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<h2>Welcome to the Meme Generator!</h2>
+			</div>
+		)
+	}
+}
+
+export default App;
