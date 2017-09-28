75b885700a6b43e20db241954da69e6034b90abf
Russell Sabia <rjsabia@gmail.com>
2017-09-28 14:11:05 -0500

    added image output, new imageitem component, and animations

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import MemeItem from './MemeItem';
+import '../styles/index.css';
 
 class App extends Component {
+	constructor() {
+		super();
+		
+		this.state = {
+			memeLimit: 10
+		}
+	}
+
 	render() {
 		return (
 			<div>
 				<h2>Welcome to the Meme Generator!</h2>
+				{
+					this.props.memes.slice(0, this.state.memeLimit).map((meme, index) => {
+						return (
+							<MemeItem key={index} meme={meme} />
+						)
+					})
+				}
+				<div className="meme-button"
+					onClick={() => {
+						this.setState({memeLimit: this.state.memeLimit+10})
+					}}>
+					Load 10 more memes...
+				</div>
 			</div>
 		)
 	}
 }
 
-export default App;
+function mapStateToProps(state) {
+	return state;
+}
+
+export default connect(mapStateToProps, null)(App);
