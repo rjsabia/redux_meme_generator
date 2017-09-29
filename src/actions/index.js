37a81f1c0fd6bc429e7b28e0c31362b8e8e36feb
Russell Sabia <rjsabia@gmail.com>
2017-09-29 00:57:56 -0500

    added POST api in reducer, custom meme form, and myMeme output, app complete, from course end

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
+import { username, password } from './secrets';
+
 export const RECIEVE_MEMES = 'RECIEVE_MEMES';
+export const NEW_MEME = "NEW_MEME";
 
 function recieveMemes(json) {
 	const { memes } = json.data;
@@ -18,5 +21,38 @@ export function fetchMemes() {
 	return function(dispatch) {
 		return fetchMemesJson()
 		.then(json => dispatch(recieveMemes(json)))
+	}
+}
+
+function newMeme(meme) {
+	return {
+		type: NEW_MEME,
+		meme
+	}
+}
+
+function postMemeJson(params) {
+	params["username"] = username;
+	params["password"] = password;
+
+	const bodyParams = Object.keys(params).map(key => {
+		return encodeURIComponent(key) + '=' + encodeURIComponent(params[key])
+	}).join('&');
+
+	console.log('bodyParams', bodyParams);
+
+	return fetch('https://api.imgflip.com/caption_image', {
+		method: "POST",
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		body: bodyParams
+	}).then(response => response.json());
+}
+
+export function createMeme(new_meme_object) {
+	return function(dispatch) {
+		return postMemeJson(new_meme_object)
+		.then(new_meme => dispatch(newMeme(new_meme)))
 	}
 }
--- src/components/MemeItem.jsx
+++ src/components/MemeItem.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createMeme } from '../actions';
 
 class MemeItem extends Component {
 	constructor() {
@@ -9,12 +11,24 @@ class MemeItem extends Component {
 		}
 	}
 
+	postMeme() {
+		console.log('this.props', this.props);
+		const { text0, text1 } = this.props;
+		const memeObj = {
+			template_id: this.props.meme.id,
+			text0,
+			text1
+		}
+		this.props.createMeme(memeObj);
+	}
+
 	render() {
 		return (
 			<div 
 				className="meme-item"
 				onMouseEnter={() => this.setState({ hovered: true })}
 				onMouseLeave={() => this.setState({ hovered: false })}
+				onClick={() => this.postMeme()}
 			>
 				<img 
 					src={this.props.meme.url}
@@ -29,4 +43,4 @@ class MemeItem extends Component {
 	}
 }
 
-export default MemeItem;
+export default connect(null, { createMeme })(MemeItem);
